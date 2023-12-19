62e9ce97d6e58fa2742296c74d64eaf164e98672
Farid Rehila <>
2023-12-19 17:21:31 +0100

    3. Images

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,18 @@
 import Link from "next/link";
+import myImage from "../../public/myimage.jpg";
+import Image from "next/image";
+
+const link =
+  "https://www.aplyca.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fc63hsprlvlya%2FIacLLeOBR5WCvdCPqKuff%2F6860b5cc464c4f54703a2befa3f706b4%2Fnextjs3.webp&w=2048&q=75";
 
 export default function DashboardPage() {
   return (
     <div>
       DashboardPage
-      <div>
-        <Link href="/">Home</Link>
-      </div>
-      <div>
-        <Link href="/login">Login</Link>
+      <Image src={myImage} alt="my image" />
+      <Image src={link} width={200} height={200} alt="hello" />
+      <div className="relative h-60 w-60 mt-10 mb-10">
+        <Image src={link} fill alt="hello" />
       </div>
     </div>
   );
